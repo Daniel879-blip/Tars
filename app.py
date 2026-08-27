39a9f48b8cff4d8b04206b08fe26e78aa35eb970
Daniel879-blip <happinessd472@gmail.com>
2026-08-27 20:12:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -970,10 +970,10 @@ def home_page():
             st.session_state.show_login = False
             st.rerun()
     with c2:
-        if st.button("Sign in", use_container_width=True):
-    st.session_state.show_login = True
-    st.session_state.show_signup = False
-    st.rerun()
+            if st.button("Sign in", use_container_width=True):
+        st.session_state.show_login = True
+        st.session_state.show_signup = False
+        st.rerun()
 
     st.write("")
     a, b, c = st.columns(3)
